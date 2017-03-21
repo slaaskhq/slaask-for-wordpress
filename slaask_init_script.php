76e84976cf9bd1fb4514d6149a40df0db8381250
Rémi Delhaye <remi@slaask.com>
2017-03-21 18:21:41 +0100

    Async

--- slaask_init_script.php
+++ slaask_init_script.php
@@ -9,5 +9,19 @@
 header("Content-type: text/javascript");
 
 if($api_key = isset($_GET['api_key']) ? $_GET['api_key'] : false){
-    echo '_slaask.init("' . $api_key . '");';
+    echo '(function() {
+  var slk = document.createElement("script");
+  slk.src = "https://cdn.slaask.com/chat.js";
+  slk.type = "text/javascript";
+  slk.async = "true";
+  slk.onload = slk.onreadystatechange = function() {
+    var rs = this.readyState;
+    if (rs && rs != "complete" && rs != "loaded") return;
+    try {
+      _slaask.init("" . $api_key . "");
+    } catch (e) {}
+  };
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(slk, s);
+})();';
 }
